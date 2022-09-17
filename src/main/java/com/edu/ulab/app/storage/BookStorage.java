4a055d998680b65c24616ac1cf9e93f69eb5369d
klen_ars <klenars@yandex.ru>
2022-09-17 11:04:04 +0300

    #1 Initial branch

--- src/main/java/com/edu/ulab/app/storage/BookStorage.java
+++ src/main/java/com/edu/ulab/app/storage/BookStorage.java
@@ -1,4 +1,7 @@
 package com.edu.ulab.app.storage;
 
+import com.edu.ulab.app.entity.Book;
+
 public interface BookStorage {
+    Book getById(long id);
 }
--- src/main/java/com/edu/ulab/app/storage/impl/BookStorageImpl.java
+++ src/main/java/com/edu/ulab/app/storage/impl/BookStorageImpl.java
@@ -3,4 +3,6 @@ package com.edu.ulab.app.storage.impl;
 import com.edu.ulab.app.storage.BookStorage;
 
 public class BookStorageImpl implements BookStorage {
+
+
 }
